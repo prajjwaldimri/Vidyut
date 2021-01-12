bab80f82aa1fededf10eac94d973d7e78bea8947
Prajjwal <prajjwaldimri@hotmail.com>
2021-01-12 17:04:24 +0530

    Removed vuetify's boilerplate code and added cors to server

--- backend/index.ts
+++ backend/index.ts
@@ -2,7 +2,11 @@ import swarm from "discovery-swarm";
 import getPort from "get-port";
 
 import express from "express";
+import cors from "cors";
 const app = express();
+
+app.use(cors());
+
 
 app.get("/", (req, res) => {
   res.send("Hello");
