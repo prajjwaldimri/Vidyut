f7f3d3a82ccd076cdb5d6068d9b94feac9d5f1c1
Prajjwal <prajjwaldimri@hotmail.com>
2021-01-12 16:34:34 +0530

    Added express to backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,14 @@
 import swarm from "discovery-swarm";
 import getPort from "get-port";
 
-let swarmConnect = async () => {
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+(async () => {
   let sw = swarm();
   sw.listen(await getPort({ port: getPort.makeRange(30000, 50000) }));
   sw.join("Vidyut");
@@ -10,4 +17,8 @@ let swarmConnect = async () => {
     console.log("found");
     console.log(sw.connected);
   });
-};
+
+  app.listen(3333, () => {
+    console.log("Express listening at 3333");
+  });
+})();
