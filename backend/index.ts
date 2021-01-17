2da29d6a21dbd5f93fde77dad644480bedf13f6c
Prajjwal <prajjwaldimri@hotmail.com>
2021-01-17 17:14:42 +0530

    Added discover swarm

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,16 @@
 
-import wrtc from "wrtc";
-import fetch from "node-fetch";
-import WebSocket from "ws";
-import SimplePeerJs from "simple-peerjs";
+// import wrtc from "wrtc";
+// import fetch from "node-fetch";
+// import WebSocket from "ws";
+// import SimplePeerJs from "simple-peerjs";
+
+import Swarm from 'discovery-swarm';
+import defaults from 'dat-swarm-defaults';
+import crypto from 'crypto';
 
 import express from "express";
 import cors from "cors";
+import getPort from 'get-port';
 const app = express();
 
 app.use(cors());
@@ -16,19 +21,59 @@ app.get("/", (req, res) => {
 });
 
 (async () => {
-  const peer = new SimplePeerJs({ id: "Vidyut-Initiator", wrtc, fetch, WebSocket });
+  // const peer = new SimplePeerJs({ id: "Vidyut-Initiator", wrtc, fetch, WebSocket });
 
-  peer.on('connect', conn => {
-    console.log('Peer connected:', conn.peerId);
+  // peer.on('connect', conn => {
+  //   console.log('Peer connected:', conn.peerId);
 
-    conn.peer.on('data', data => {
-      console.log(data.toString());
+  //   conn.peer.on('data', data => {
+  //     console.log(data.toString());
+  //   });
+  // });
+
+  const peers = {};
+  let connSeq = 0;
+  let channel = 'Vidyut';
+
+  const myPeerId = crypto.randomBytes(32);
+  console.log('MyPeerId: ' + myPeerId.toString('hex'));
+
+  const config = defaults({ id: myPeerId });
+  const swarm = Swarm(config);
+  const port = await getPort();
+
+  swarm.listen(port);
+  console.log(`Listening on ${port}`);
+  swarm.join(channel, { announce: true });
+  swarm.on('connection', (conn, info) => {
+    console.log(conn);
+    console.log(info);
+    const seq = connSeq;
+    const peerId = info.id.toString('hex');
+    if (info.initiator) {
+      try {
+        conn.setKeepAlive(true, 600);
+      } catch (exception) {
+        console.log("Error", exception);
+      }
+    }
+
+    conn.on('close', () => {
+      console.log(`Connection ${seq} closed`);
     });
   });
 
-
-  app.listen(3333, () => {
-    console.log("Express listening at 3333");
+  swarm.on("peer-banned", () => {
+    console.log("Banned");
   });
+
+  swarm.on("peer-rejected", () => {
+    console.log("Rejected");
+  });
+
+
+  // app.listen(3333, () => {
+  //   console.log("Express listening at 3333");
+  // });
 })();
 
